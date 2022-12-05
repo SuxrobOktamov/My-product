c6f67a3328dd5979d28546f44b81c3c5c3636e35
Suxrob Oktamov <oktamovsuxrob0@gmail.com>
2022-12-05 08:53:41 +0500

    new files added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+fonts = document.querySelector("#dark-light");
+
+fonts.addEventListener('click', function(){
+    document.body.classList.toggle('dark');
+});
+
+font = document.querySelector("#dark-lights");
+
+font.addEventListener('click', function(){
+    document.body.classList.toggle('dark');
+});
